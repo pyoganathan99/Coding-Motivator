861a93c704a8124ef6181287449a19f8f8a6bafe
yoganathan.p <yoganathan.p@zohocorp.com>
2021-03-13 09:16:32 +0530

    add messages

--- src/messages.ts
+++ src/messages.ts
@@ -1,6 +1,25 @@
 const motivationData = {
     messages: [
-        'You are doing awesome! Keep rocking',
+        `You are doing awesome! Keep rocking`,
+        `Push yourself, because no one is going to give you their git credentials`,
+        `[Warning] Don't use light mode, it attracts bugs!`,
+        `Some good coding you got there, keep it up!`,
+        `Drink water! Hydro gang!`,
+        `Gentle reminder to check your posture`,
+        `Sit back and relax a few minutes, you deserve it 😎`,
+        `Don't give up... Keep trying!`,
+        `You are doing a good job. * pat pat *`,
+        `Patience + Consistency is the key`,
+        `Don't stop, keep pusing yourself!`,
+        `Have you been sitting too long? Maybe take a break? 😇`,
+        `Get those bugs! 🔫 pew pew pew`,
+        `Gentle reminder to stay hydrated 🥤`,
+        `“The secret of getting ahead is getting started.” – Mark Twain`,
+        `✨ B R E A T H E ✨ Take a moment to relax`,
+        `Don't give those bugs a chance 😎`,
+        `Time is your most valuable resource, spend mindfully 😁`,
+        `Test before your deploy 👀 Gotta look out for those sneaky bugs`,
+        `Gentle reminder to sit back and relax yourself 😌`
     ]
 };
 
